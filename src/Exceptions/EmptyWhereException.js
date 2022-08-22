d951c49209901a26f108405c9f679ebbc5b38158
João Lenon <lenonSec7@gmail.com>
2022-08-21 23:36:19 -0300

    fix(exceptions): better content/help in exceptions

--- src/Exceptions/EmptyWhereException.js
+++ src/Exceptions/EmptyWhereException.js
@@ -14,17 +14,16 @@ export class EmptyWhereException extends Exception {
    * Creates a new instance of EmptyWhereException.
    *
    * @param {string} method
-   * @param {string} fallbackMethod
    * @return {EmptyWhereException}
    */
-  constructor(method, fallbackMethod) {
+  constructor(method) {
     const content = `Be careful, you are trying to call "${method}" method with empty "where" object.`
 
     super(
       content,
       500,
       'E_EMPTY_WHERE_ERROR',
-      `If you really want to do that use "${fallbackMethod}" method instead.`,
+      `If you really want to do that use you will need to use the default TypeORM query builder. You can do that using the "database.query()" method.`,
     )
   }
 }
