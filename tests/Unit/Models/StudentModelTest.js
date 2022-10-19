9ecf45c6644203b6bdc0145fbecf55e1cb222c46
João Lenon <lenonSec7@gmail.com>
2022-10-19 09:35:39 -0300

    test(model): add validation in extras ids

--- tests/Unit/Models/StudentModelTest.js
+++ tests/Unit/Models/StudentModelTest.js
@@ -53,6 +53,7 @@ test.group('StudentModelTest', group => {
 
     await student.save()
 
+    assert.isDefined(student.$extras.id)
     assert.equal(course.id, student.$extras[0].courseId)
     assert.equal(student.id, student.$extras[0].studentId)
     assert.deepEqual(student.$extras, await DB.connection('mysql').table('students_courses').findMany())
